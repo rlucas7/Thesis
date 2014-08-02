fb0f899fb64281c2c69f781097d04b76dfd3b7cc
Lucas Roberts <rlucas7@vt.edu>
2014-08-01 20:36:37 -0400

    added code to run the simulations comparing divas and alovas and other models.

--- latex/comparing_alovas_n_divas/comparE_alovas_divas_first.R
+++ latex/comparing_alovas_n_divas/comparE_alovas_divas_first.R
@@ -25,34 +25,308 @@ for(i in 1:n){
 }# end of for i in 1:n looop
 
 lm_mod <- lm(Y~X)
-X<-X[,1:(p-1)]
 tree_mod <- tree(Y~X)
-
+getwd()
+pdf(file="compare_divas_alovas.pdf")
+par(fin=c(7,7))
 plot(tree_mod,type='uniform')
 text(tree_mod)
-
+dev.off()
 ## now if the interaction is ommitted 
 
 X<-X[,1:(p-1)]
 tree_mod <- tree(Y~X)
 
+pdf(file="compare_divas_alovas2.pdf")
 plot(tree_mod,type='uniform')
 text(tree_mod)
+dev.off()
+
+### load the stuff into wd needed 
+source('/Users/rlucas7/ALoVaS/R_code/run_first_ALoVaS.r') # fur ALoVaS
+### end of load the stuff 
+
 
 
 ####===========================SOC fit the divas model=========================== 
+casper_data <- data.frame(Y,X)
+	n=1000
+	m=10
+	data_dim=101
+	
+# SETTING UP DATA STRUCTURES....
+t_lh<-matrix(NA,nrow=n*m,ncol=1) #matrix tracks the likelihood of the tree at the current iteration of the MCMC chain
+output<-matrix(NA, nrow=n*m, ncol=2)
+rtree<-vector(mode="list",length=n*m)
+
+alpha_prime<-matrix(1,nrow=data_dim)
+alpha_prime_store<-matrix(NA, nrow=n*m,ncol=length(alpha_prime))
+weight<-matrix(NA, nrow=n*m,ncol=length(alpha_prime)-1)
+count<-matrix(NA,nrow=n*m, ncol=length(alpha_prime))
+#INITIALIZATIONS
+
+t_lh[1]<-tree_lhood(t1,casper_data,theta=0,mult_penalty=FALSE)
+output[1,1]<-length(unique(t1$frame[,1]))-1
+output[1,2]<-length(get_tnodes(t1))
+rtree[[1]]<-t1
+
+Z<-matrix(NA, nrow=n*m, ncol=1) #here the "Z" vector represents the constraint multipliers $\widetilde{\alpha}$
+t2<-t1 # the "optimal" tree found by the MCMC search   ////////// ????Is this line still necessary for the code ?????
+Z[1]<-1
+### initializations 
+
+alpha_prime_store[1,]<-alpha_prime
+weight[1,]<-1/sum(alpha_prime)
+
+split_counts<-0
+
+old_count<-0
 
 
+for(j in 1:m){
+	i<-1
+	tree_num<-sample(1:hmm,1,prob=x_double_prime)
+	tree1<-tree_init_mat[[tree_num]]
+	tree2<-tree1 
+	Z[i+1000*(j-1)]<-1
+	alpha_prime_store[i+1000*(j-1) ,]<-alpha_prime
+	weight[i+1000*(j-1),]<-1/length(alpha_prime)
+ 	t_lh[i+1000*(j-1)]<-tree_lhood(tree2,casper_data,theta=0,mult_penalty=FALSE)
+ 	split_counts<-0
+for(i in 2:n){
+	x<-rgen_discrete_unif()
+			if( x == 1 ){ 
+			# sample the grow
+			tree2<-grow(tree1,casper_data,1,weight[1,])		
+			tree2<-up(tree2,casper_data,1)
+			}else{
+				if(x ==2){
+				tree2<-prune(tree1,casper_data,1)
+				tree2<-up(tree2,casper_data,1)
+			}else{ 
+				if(x==3 ){
+					tree2<-change(tree1,casper_data,1,weight[1000*(j-1)+i-1,])		
+					tree2<-up(tree2,casper_data,1)
+				}else{
+					if(x==4 ){
+						#rotate step...
+						tree2<-rotate_k(tree1)
+						tree2<-up(tree2,casper_data,1)	
+						}else{ # x==5 case
+						tree2<-swap(tree1,casper_data,1)
+						tree2<-up(tree2,casper_data,1)
+						#include swap function here, .....
+						}
+					}	
+				}
+			}
+	
+	t2_lh<-tree_lhood(tree2,casper_data,theta=0,mult_penalty=FALSE)	# calculate the proposed tree's l-hood	
+	#proposed_tlh[i+1000*(j-1)]<-t2_lh
+	### log ratio calculation 
+	
+	### old code:	
+	#R<- t2_lh - t_lh[i+1000*(j-1)-1] 
+	
+	###NEW code: 
+		### Start of likelihood ratio calculation 
+	
+	R<-log_ratio_calc(tree2,tree1,t2_lh, t_lh[i+1000*(j-1)-1],data=casper_data,weight=weight[i+1000*(j-1)-1,])
+	
+	#R<- t2_lh - c_t_lh[i+1000*(j-1)-1] 
+	###End of likelihood ration calculation
+	
+	### end dof log ratio  calculation
+	
+	M_E<- -rexp(1,1)
+	if(M_E <=R){ 
+		t_lh[i+ (j-1)*1000 ]<- t2_lh # make the move to the proposed tree
+			# stay at tree2 so do not update the tree2 object
+			tree1<-tree2
+			output[i+(j-1)*1000,1]<-length(unique(tree1$frame[,1]))-1
+			output[i+(j-1)*1000,2]<-length(get_tnodes(tree1))
+		}else{
+			# stay at the current tree state 
+			t_lh[i+(j-1)*1000]<-t_lh[1000*(j-1)+i-1]
+			output[i+(j-1)*1000,1]<-length(unique(tree1$frame[,1]))-1
+			output[i+(j-1)*1000,2]<-length(get_tnodes(tree1))			#num_term_nodes[i+(j-1)*1000-1]
+		}
+		rtree[[i+(j-1)*1000]]<-tree1
+	if(i %%300 ==0 ){
+		print(tree1)
+		}
+	
+	#### now sample from the weights for each dimension.... 
+	split_counts<-count_cov_split(tree=tree1,data=casper_data,1) + split_counts 
+	Z[(j-1)*1000+i]<- data_dim/sum(split_counts)
+	#split_counts<-(split_counts)*Z[(j-1)*1000+i]/10 # here the dividing number must change as the dimensionality of data does 
+	weight[(j-1)*1000+i,]<-sample_weights(alpha=split_counts) ### the 1's are added inside the function.... 
+
+	}# end of for(i 1:n) loop
+} #end of for j in 1:m  loop
 
 ####===========================EOC to fit the divas model===========================
 
 
 
 ####===========================SOC fit the alovas model=========================== 
+delta_grid<-seq(from=1,to=25, by=1)
+MSE_grid<-seq(from=1,to=25, by=1)
+n<-length(delta_grid)
+n
+
+results<-matrix(NA_real_, nrow=7, ncol=1)
+
+ptm <- proc.time()
+k <- 1
+j<-1
+
+for(j in 1:7){
+
+mz <-  my_cov[j,k]
+
+obj<-unbalanced(1000,num_cov=mz,one_hundredth)  
+obj1<-unbalanced(1000,num_cov=mz,one_hundredth)  
+
+
+
+for(i in 1:n){
+	dg <- delta_grid[i]
+	ltree1<-lasso_tree(sim=obj,n=1000,m=1,r_p =1, delta = dg, vocal=TRUE)
+	MSE_grid[i] <- MSE_calc(ltree1,obj)
+}
+
+mini <- match(min(MSE_grid),MSE_grid)
+optimum <- delta_grid[mini]
+# run one of these three
+ltree1 <- lasso_tree(sim=obj,n=1000,m=1,r_p =1, delta = optimum, vocal=TRUE)
+#stree1<-ssvs_tree(sim=obj,n=100,m=1,sig_sq_nz = rep(1,mz), sig_sq_z = rep(dg,mz) , vocal=TRUE)
+#htree1 <- horseshoe_tree(sim=obj,n=1000,m=1,tau_sq = optimum, vocal=TRUE)
+# store results in one of these three
+results[j,k] <- MSE_calc(ltree1,obj1)
+#results[j,k] <- MSE_calc(stree1,obj1)
+#results[j,k] <- MSE_calc(htree1,obj1)
+# write results to one of these three
+ write.csv(results, file='lasso_compare_results.csv')
+ #write.csv(results, file='lasso_compare_results.csv')
+ #write.csv(results, file='lasso_compare_results.csv')
+
+ }# end of j loop
+ 
+proc.time() - ptm
+system('echo "this goes into email" | mail "done" rlucas7@vt.edu' )
+
+getwd()
+setwd("/Users/rlucas7/thesis/")
+
+pdf(file='elbow_plot_lasso_compare.pdf')
+
+plot(delta_grid,MSE_grid/100,type='l')
+dev.off()
 
 
 
 ####===========================EOC to fit the alovas model===========================
+
+
+####===========================SOC to fit the cgm model===========================
+casper_data <- data.frame(Y,X)
+	n=1000
+	m=10
+	data_dim=101
+	
+# SETTING UP DATA STRUCTURES....
+t_lh<-matrix(NA,nrow=n*m,ncol=1) #matrix tracks the likelihood of the tree at the current iteration of the MCMC chain
+output<-matrix(NA, nrow=n*m, ncol=2)
+rtree<-vector(mode="list",length=n*m)
+
+#INITIALIZATIONS
+
+t_lh[1]<-tree_lhood(t1,casper_data,theta=0,mult_penalty=FALSE)
+output[1,1]<-length(unique(t1$frame[,1]))-1
+output[1,2]<-length(get_tnodes(t1))
+rtree[[1]]<-t1
+
+t2<-t1 # the "optimal" tree found by the MCMC search   ////////// ????Is this line still necessary for the code ?????
+
+for(j in 1:m){
+	i<-1
+	tree_num<-sample(1:hmm,1,prob=x_double_prime)
+	tree1<-tree_init_mat[[tree_num]]
+	tree2<-tree1 
+	Z[i+1000*(j-1)]<-1
+	alpha_prime_store[i+1000*(j-1) ,]<-alpha_prime
+	weight[i+1000*(j-1),]<-1/length(alpha_prime)
+ 	t_lh[i+1000*(j-1)]<-tree_lhood(tree2,casper_data,theta=0,mult_penalty=FALSE)
+ 	split_counts<-0
+for(i in 2:n){
+	x<-rgen_discrete_unif()
+			if( x == 1 ){ 
+			# sample the grow
+			tree2<-grow(tree1,casper_data,1,weight[1,])		
+			tree2<-up(tree2,casper_data,1)
+			}else{
+				if(x ==2){
+				tree2<-prune(tree1,casper_data,1)
+				tree2<-up(tree2,casper_data,1)
+			}else{ 
+				if(x==3 ){
+					tree2<-change(tree1,casper_data,1,weight[1000*(j-1)+i-1,])		
+					tree2<-up(tree2,casper_data,1)
+				}else{
+					if(x==4 ){
+						#rotate step...
+						tree2<-rotate_k(tree1)
+						tree2<-up(tree2,casper_data,1)	
+						}else{ # x==5 case
+						tree2<-swap(tree1,casper_data,1)
+						tree2<-up(tree2,casper_data,1)
+						#include swap function here, .....
+						}
+					}	
+				}
+			}
+	
+	t2_lh<-tree_lhood(tree2,casper_data,theta=0,mult_penalty=FALSE)	# calculate the proposed tree's l-hood	
+	#proposed_tlh[i+1000*(j-1)]<-t2_lh
+	### log ratio calculation 
+	
+	### old code:	
+	#R<- t2_lh - t_lh[i+1000*(j-1)-1] 
+	
+	###NEW code: 
+		### Start of likelihood ratio calculation 
+	
+	R<-log_ratio_calc(tree2,tree1,t2_lh, t_lh[i+1000*(j-1)-1],data=casper_data,weight=weight[i+1000*(j-1)-1,])
+	
+	#R<- t2_lh - c_t_lh[i+1000*(j-1)-1] 
+	###End of likelihood ration calculation
+	
+	### end dof log ratio  calculation
+	
+	M_E<- -rexp(1,1)
+	if(M_E <=R){ 
+		t_lh[i+ (j-1)*1000 ]<- t2_lh # make the move to the proposed tree
+			# stay at tree2 so do not update the tree2 object
+			tree1<-tree2
+			output[i+(j-1)*1000,1]<-length(unique(tree1$frame[,1]))-1
+			output[i+(j-1)*1000,2]<-length(get_tnodes(tree1))
+		}else{
+			# stay at the current tree state 
+			t_lh[i+(j-1)*1000]<-t_lh[1000*(j-1)+i-1]
+			output[i+(j-1)*1000,1]<-length(unique(tree1$frame[,1]))-1
+			output[i+(j-1)*1000,2]<-length(get_tnodes(tree1))			#num_term_nodes[i+(j-1)*1000-1]
+		}
+		rtree[[i+(j-1)*1000]]<-tree1
+	if(i %%300 ==0 ){
+		print(tree1)
+		}
+	}# end of for(i 1:n) loop
+} #end of for j in 1:m  loop
+
+
+####===========================EOC to fit the cgm model===========================
+
 
 
 ####===========================SOC to do the hold out error calc=========================== 
@@ -71,12 +345,7 @@ for(i in 1:n){
 	
 }# end of for i in 1:n looop
 
-lm_mod2 <- lm(Y~X)
-X<-X[,1:(p-1)]
-tree_mod2 <- tree(Y~X)
 
-plot(tree_mod2,type='uniform')
-text(tree_mod2)
 
 ### now write the code to calculate the misclass/hould-out error 
 
