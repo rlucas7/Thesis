375c7f4b47183dc1f9ed85b83e8a81b5b0264269
Lucas Roberts <rlucas7@vt.edu>
2014-09-02 20:58:52 -0400

    made changes to figures, formatting and pagebreak between chapters. Almost done

--- plot_codes/phase_change_shatter_dynamics.R
+++ plot_codes/phase_change_shatter_dynamics.R
@@ -3,10 +3,15 @@
 
 n<-seq(0,20, by=0.01)
 fn<-2^n
-for(i in 1001:length(x)){
+for(i in 1001:length(n)){
 	fn[i]<-2^10+50*n[i]
 }
 getwd()
-pdf(file="phase_change_shatter_dynamics.pdf")
-plot(n,fn,type="l", main="Shatter Coefficient growth with n")
+setwd("/Users/rlucas7/Desktop")
+pdf(file="phase_change_shatter_dynamics3.pdf")
+plot(n,fn,type="l", ylab="Shatter Coefficient growth with n")
+segments(0,1535,10,1525,lty=2)
+segments(10.05,0,10.05,1525,lty=2)
+#arrows(15,1000,10.2,10)
+#text(15,1030,'VC dimension')
 dev.off()
